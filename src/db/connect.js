2d6428714e81e6bc1df8afee01fb26dd51f56276
Sumit31-cyber <sumitkum9305@gmail.com>
2021-12-29 20:27:17 +0530

    database changed

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/olympics', {
+DB = 'mongodb://localhost:27017/olympics',
+mongoose.connect(DB, {
 
     useNewUrlParser:true,
     useUnifiedTopology:true
